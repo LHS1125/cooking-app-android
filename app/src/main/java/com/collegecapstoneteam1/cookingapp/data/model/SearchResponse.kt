02feb86aa161240c815d0b03faff9123cab7170e
LHS1125 <gh1369tjd@gmail.com>
2022-10-04 11:36:10 +0900

    Revert "Merge pull request #1 from PotatoMeme/sampleBase"
    This reverts commit 4206e3d024341a6f840a22df919ad12e01486741, reversing
    changes made to 07423bc05ef4a248e7aca94677dc701336b256ae.

--- app/src/main/java/com/collegecapstoneteam1/cookingapp/data/model/SearchResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.collegecapstoneteam1.cookingapp.data.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class SearchResponse(
-    @SerializedName("COOKRCP01")
-    val cOOKRCP01: COOKRCP01
-)
